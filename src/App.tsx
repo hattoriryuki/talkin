b91f808747db7532e034caee4468381d8ebfa8a7
hattori ryuki <ryuki.97913795@i.softbank.jp>
2023-08-31 21:54:09 +0900

    feat: ヘッダー、フッターの雛形を作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  Box,
+  ChakraProvider,
+  Container,
+  Flex,
+  Heading,
+  Stack,
+  Text
+} from '@chakra-ui/react';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ChakraProvider>
+      <Flex
+        as="nav"
+        bg="teal.500"
+        color="gray.50"
+        align="center"
+        justify="space-between"
+        padding={3}
+        h="60px"
+      >
+        <Heading
+          fontWeight={900}
+          letterSpacing="-0.07em"
+          transform="scale(1, 0.7)"
+          ml={6}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          TALKIN
+        </Heading>
+      </Flex>
+      <Box
+        bg="teal.500"
+        color="gray.50"
+        position="fixed"
+        bottom="0"
+        w="100%"
+      >
+        <Container
+          as={Stack}
+          maxW={"6xl"}
+          py={4}
+          direction="row"
+          spacing={4}
+          justify="center"
+          align="center"
+          gap={20}
+        >
+          <Stack direction={"row"} spacing={6}>
+            <Box as="a" href={"#"}>
+              利用規約
+            </Box>
+          </Stack>
+          <Text>©︎ 2023 TALKIN. All rights reserved</Text>
+        </Container>
+      </Box>
+    </ChakraProvider>
   );
 }
 
