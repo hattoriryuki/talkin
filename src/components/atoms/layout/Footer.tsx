8f3773ee8aab30170be0b1606f588ac368d64967
hattori ryuki <ryuki.97913795@i.softbank.jp>
2023-09-01 21:35:01 +0900

    refactor: 誤記を修正

--- src/components/atoms/layout/Footer.tsx
+++ src/components/atoms/layout/Footer.tsx
@@ -4,20 +4,21 @@ import { Box, Container, Stack, Text } from "@chakra-ui/react";
 export const Footer: FC = memo(() => {
   return (
     <Box
-    bg="teal.500"
-    color="gray.50"
-    position="fixed"
-    bottom="0"
-    w="100%"
+    bg={"teal.500"}
+    color={"gray.50"}
+    position={"fixed"}
+    bottom={"0"}
+    w={"100%"}
+    h={"50px"}
     >
       <Container
         as={Stack}
         maxW={"6xl"}
         py={4}
-        direction="row"
+        direction={"row"}
         spacing={4}
-        justify="center"
-        align="center"
+        justify={"center"}
+        align={"center"}
         gap={20}
       >
         <Stack direction={"row"} spacing={6}>
