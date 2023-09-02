f5b8f9eac39a4ac968a59a1f57d437447eff8bc0
hattori ryuki <ryuki.97913795@i.softbank.jp>
2023-09-02 18:12:26 +0900

    refactor: ボタンをコンポーネント化

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {
   Avatar,
   Box,
-  Button,
   ChakraProvider,
   Container,
   Flex,
@@ -14,58 +13,59 @@ import {
 
 import { DefaultLayout } from './components/templates/DefaultLayout';
 import speechBubbleImage from "./assets/images/speech-bubble.png";
+import { PrimaryButton } from './components/atoms/button/PrimaryButton';
 
 function App() {
   return (
     <ChakraProvider>
       <DefaultLayout>
         <Flex
-          h={"calc(100vh - 110px)"}
-          justify={"flex-start"}
-          color={"gray.700"}
+          h="calc(100vh - 110px)"
+          justify="flex-start"
+          color="gray.700"
         >
           <Container
             as={Flex}
-            direction={"column"}
-            justify={"space-between"}
-            h={"100%"}
+            direction="column"
+            justify="space-between"
+            h="100%"
           >
             <Box my={4} flex={0.3}>
               <Heading>とーきんへようこそ!</Heading>
-              <Text w={"480px"} mt={4} fontSize={"lg"}>
+              <Text w="480px" mt={4} fontSize="lg">
                 このサービスは、気軽に匿名でチャットができるサービスです。
                 面倒なユーザー登録等は必要ありません。<br />
                 誹謗中傷等、相手を傷つける行為は全て禁止いたします。
               </Text>
             </Box>
-            <Flex flex={0.7} align={"end"}>
+            <Flex flex={0.7} align="end">
               <Box
                 as={Flex}
-                direction={"column"}
-                border={"1px black solid"}
-                flex={"0.5"}
+                direction="column"
+                border="1px black solid"
+                flex="0.5"
                 m={8}
                 h={250}
-                position={"relative"}
+                position="relative"
                 bottom={180}
-                justify={"space-between"}
+                justify="space-between"
                 py={4}
               >
                 <Image src={speechBubbleImage}
                   alt=""
-                  boxSize={"200px"}
-                  mx={"auto"}
+                  boxSize="200px"
+                  mx="auto"
                 />
                 <Text
-                  align={"center"}
-                  position={"absolute"}
-                  top={"30%"}
-                  left={"30%"}
+                  align="center"
+                  position="absolute"
+                  top="30%"
+                  left="30%"
                 >
                   ヾ(●´ω｀●)
                 </Text>
-                <Flex align={"center"} ml={4}>
-                  <Avatar name={"taro"} />
+                <Flex align="center" ml={4}>
+                  <Avatar name="taro" />
                   <Text ml={2}>
                     太郎
                   </Text>
@@ -73,30 +73,30 @@ function App() {
               </Box>
               <Box
                 as={Flex}
-                direction={"column"}
-                border={"1px black solid"}
-                flex={"0.5"}
+                direction="column"
+                border="1px black solid"
+                flex="0.5"
                 m={8}
                 h={250}
-                justify={"space-between"}
+                justify="space-between"
                 py={6}
-                position={"relative"}
+                position="relative"
               >
                 <Image src={speechBubbleImage}
                   alt=""
-                  boxSize={"200px"}
-                  mx={"auto"}
+                  boxSize="200px"
+                  mx="auto"
                 />
                 <Text
-                  align={"center"}
-                  position={"absolute"}
-                  top={"30%"}
-                  left={"30%"}
+                  align="center"
+                  position="absolute"
+                  top="30%"
+                  left="30%"
                 >
                   こんにちは
                 </Text>
-                <Flex align={"center"} ml={4}>
-                  <Avatar name={"jiro"} />
+                <Flex align="center" ml={4}>
+                  <Avatar name="jiro" />
                   <Text ml={2}>
                     二郎
                   </Text>
@@ -105,25 +105,19 @@ function App() {
             </Flex>
           </Container>
           <Flex
-            direction={"column"}
-            justify={"end"}
-            align={"end"}
-            h={"100%"}
+            direction="column"
+            justify="end"
+            align="end"
+            h="100%"
             p={4}
-            pb={"32px"}
-            mr={"6"}
+            pb="32px"
+            mr={6}
           >
-            <Text fontWeight={"bold"}>
+            <Text fontWeight="bold">
               チャットで使用する名前を入力して、はじめるボタンを押してください
             </Text>
-            <Input placeholder={"太郎"} m={2} />
-            <Button
-              bg={"teal.400"}
-              color={"white"}
-              _hover={{ opacity: 0.8 }}
-            >
-              はじめる
-            </Button>
+            <Input placeholder="太郎" m={2} />
+            <PrimaryButton>はじめる</PrimaryButton>
           </Flex>
         </Flex>
       </DefaultLayout>
