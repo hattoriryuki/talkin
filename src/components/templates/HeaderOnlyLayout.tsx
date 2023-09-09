83bb23659db97f03db877437f2be77d66037db4b
hattori ryuki <ryuki.97913795@i.softbank.jp>
2023-09-09 17:30:06 +0900

    feat: 退室ボタンでDBからユーザーを削除する機能を追加

--- src/components/templates/HeaderOnlyLayout.tsx
+++ src/components/templates/HeaderOnlyLayout.tsx
@@ -1,10 +1,13 @@
 import { FC, ReactNode, memo, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
-import { useSetRecoilState } from "recoil";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 
 import { Header } from "../atoms/layout/Header";
 import { PrimaryButton } from "../atoms/button/PrimaryButton";
 import { authState } from "../../store/authState";
+import { deleteDoc, doc } from "firebase/firestore";
+import { db } from "../../firebase";
+import { userState } from "../../store/userState";
 
 type Props = {
   children: ReactNode;
@@ -14,8 +17,10 @@ export const HeaderOnlyLayout: FC<Props> = memo((props) => {
   const { children } = props;
   const navigate = useNavigate();
   const setUserInfo = useSetRecoilState(authState);
+  const userInfo = useRecoilValue(userState);
 
-  const onClickTop = useCallback(() => {
+  const onClickTop = useCallback(async () => {
+    await deleteDoc(doc(db, "users", userInfo.uuid));
     setUserInfo({ isAuth: false });
     navigate("/");
   }, []);
