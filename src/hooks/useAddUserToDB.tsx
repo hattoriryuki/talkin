a3e469a5a4a40036b3785a96ceb67beb77f09f85
hattori ryuki <ryuki.97913795@i.softbank.jp>
2023-09-09 15:27:43 +0900

    fix: チャットルームでUUIDが不一致になるバグを修正

--- src/hooks/useAddUserToDB.tsx
+++ src/hooks/useAddUserToDB.tsx
@@ -1,21 +1,20 @@
 import { useCallback } from "react";
 import { doc, serverTimestamp, setDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
-import { useRecoilState, useSetRecoilState } from "recoil";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 
 import { db } from "../firebase";
 import { authState } from "../store/authState";
 import { userState } from "../store/userState";
 
 export const useAddUserToDB = (userName: string) => {
-  const [userInfo, setUserInfo] = useRecoilState(userState);
+  const userInfo = useRecoilValue(userState);
   const setAuthInfo = useSetRecoilState(authState);
   const navigate = useNavigate();
 
   const addUserToDB = useCallback(async () => {
     if(userName) {
       setAuthInfo({ isAuth: true });
-      setUserInfo({ uuid: crypto.randomUUID() });
       await setDoc(doc(db, "users", userInfo.uuid), {
         name: userName,
         message: "",
