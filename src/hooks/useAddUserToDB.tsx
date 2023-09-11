6fca3368d8cbcc7156fe97caa52a64ef32d4776b
hattori ryuki <ryuki.97913795@i.softbank.jp>
2023-09-11 12:16:44 +0900

    feat: トースト機能を追加

--- src/hooks/useAddUserToDB.tsx
+++ src/hooks/useAddUserToDB.tsx
@@ -2,15 +2,17 @@ import { useCallback } from "react";
 import { doc, serverTimestamp, setDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 import { useRecoilValue, useSetRecoilState } from "recoil";
-
 import { db } from "../firebase";
+
 import { authState } from "../store/authState";
 import { userState } from "../store/userState";
+import { useToastMsg } from "./useToastMsg";
 
 export const useAddUserToDB = (userName: string) => {
   const userInfo = useRecoilValue(userState);
   const setAuthInfo = useSetRecoilState(authState);
   const navigate = useNavigate();
+  const { showToastMsg } = useToastMsg();
 
   const addUserToDB = useCallback(async () => {
     if(userName) {
@@ -23,7 +25,10 @@ export const useAddUserToDB = (userName: string) => {
       });
       navigate("/chatroom");
     } else {
-      alert("ユーザー名を入力してください");
+      showToastMsg({
+        title: "ユーザー名を入力してください",
+        status: "error"
+      });
     }
   }, [userName, userInfo]);
   
