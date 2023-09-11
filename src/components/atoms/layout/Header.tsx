ccfc391ae45a3ed8e8c94e8e7913683da4998e92
hattori ryuki <ryuki.97913795@i.softbank.jp>
2023-09-11 12:17:33 +0900

    feat: ヘッダーロゴにトップページへのリンクを追加

--- src/components/atoms/layout/Header.tsx
+++ src/components/atoms/layout/Header.tsx
@@ -1,5 +1,6 @@
-import { FC, ReactNode, memo } from "react";
+import { FC, ReactNode, memo, useCallback } from "react";
 import { Flex, Heading } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   children?: ReactNode;
@@ -7,6 +8,9 @@ type Props = {
 
 export const Header: FC<Props> = memo((props) => {
   const { children } = props;
+  const navigate = useNavigate();
+
+  const onClickTop = useCallback(() => navigate("/"), []);
 
   return (
     <Flex
@@ -24,6 +28,8 @@ export const Header: FC<Props> = memo((props) => {
         letterSpacing="-0.07em"
         transform="scale(1, 0.7)"
         ml={6}
+        cursor="pointer"
+        onClick={onClickTop}
       >
         TALKIN
       </Heading>
