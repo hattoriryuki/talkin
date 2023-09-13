8b3607bcd2df2a71d44704b7fb1e793096fe2f54
hattori ryuki <ryuki.97913795@i.softbank.jp>
2023-09-13 21:42:44 +0900

    feat: 404ページの雛形を追加

--- src/components/pages/Page404.tsx
+++ src/components/pages/Page404.tsx
@@ -1,11 +1,40 @@
 import { FC, memo } from "react";
-import { Box, Container, Heading } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Flex,
+  Heading,
+  Link
+} from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
 export const Page404: FC = memo(() => {
+  const navigate = useNavigate();
+
+  const onClickTop = () => navigate("/");
+
   return (
-    <Container>
-      <Box>
-        <Heading>Error 404</Heading>
+    <Container
+      h="100vh"
+      color="#888"
+    >
+      <Box
+        as={Flex}
+        h="100%"
+        direction="column"
+        justify="center"
+        align="center"
+      >
+        <Heading
+          fontSize="6xl"
+          fontFamily="Lato, sans-serif"
+          mb={8}
+        >
+          Error 404
+        </Heading>
+        <Link onClick={onClickTop}>
+          トップへ
+        </Link>
       </Box>
     </Container>
   );
