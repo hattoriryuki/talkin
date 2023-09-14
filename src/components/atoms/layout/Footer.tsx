645bd9e02a3cc765226c1313b2db1554e75517b4
hattori ryuki <ryuki.97913795@i.softbank.jp>
2023-09-14 21:56:11 +0900

    feat: 利用規約モーダルの雛形を作成

--- src/components/atoms/layout/Footer.tsx
+++ src/components/atoms/layout/Footer.tsx
@@ -1,10 +1,14 @@
 import { FC, memo } from "react";
 import {
   Box,
+  Button,
   Container,
   Modal,
   ModalBody,
+  ModalCloseButton,
   ModalContent,
+  ModalFooter,
+  ModalHeader,
   ModalOverlay,
   Stack,
   Text,
@@ -38,10 +42,22 @@ export const Footer: FC = memo(() => {
           <Text>&copy; 2023 TALKIN. All rights reserved</Text>
         </Container>
       </Box>
-      <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        motionPreset="slideInBottom"
+        scrollBehavior="inside"
+      >
         <ModalOverlay />
         <ModalContent>
+          <ModalHeader>利用規約</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
             <p>test</p>
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>Close</Button>
+          </ModalFooter>
         </ModalContent>
       </Modal>
     </>
