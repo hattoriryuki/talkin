366cb19d0cffd3219f8a3f710d473050476dede6
hattori ryuki <ryuki.97913795@i.softbank.jp>
2023-09-14 21:42:05 +0900

    feat: フッターの利用規約にモーダル機能のロジックを追加

--- src/components/atoms/layout/Footer.tsx
+++ src/components/atoms/layout/Footer.tsx
@@ -2,34 +2,48 @@ import { FC, memo } from "react";
 import {
   Box,
   Container,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalOverlay,
   Stack,
-  Text
+  Text,
+  useDisclosure
 } from "@chakra-ui/react";
 
 export const Footer: FC = memo(() => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <Box
-      bg="teal.500"
-      color="gray.50"
-      position="fixed"
-      bottom="0"
-      w="100%"
-      h="50px"
-    >
-      <Container
-        as={Stack}
-        maxW="6xl"
-        py={4}
-        direction="row"
-        justify="center"
-        align="center"
-        gap={{ base: "4", md: "20" }}
+    <>
+      <Box
+        bg="teal.500"
+        color="gray.50"
+        position="fixed"
+        bottom="0"
+        w="100%"
+        h="50px"
       >
-        <Box as="a" href="#">
-          利用規約
-        </Box>
-        <Text>&copy; 2023 TALKIN. All rights reserved</Text>
-      </Container>
-    </Box>
+        <Container
+          as={Stack}
+          maxW="6xl"
+          py={4}
+          direction="row"
+          justify="center"
+          align="center"
+          gap={{ base: "4", md: "20" }}
+        >
+          <Box as="a" href="#" onClick={onOpen}>
+            利用規約
+          </Box>
+          <Text>&copy; 2023 TALKIN. All rights reserved</Text>
+        </Container>
+      </Box>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+            <p>test</p>
+        </ModalContent>
+      </Modal>
+    </>
   );
 });
