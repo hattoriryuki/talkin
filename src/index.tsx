07f49b29e690ba15b5488e719092ef46a10f70f1
hattori ryuki <ryuki.97913795@i.softbank.jp>
2023-09-21 22:31:22 +0900

    refactor: 機能の見直しで不要となったreact-helmet-asyncを削除

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { HelmetProvider } from 'react-helmet-async';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
+    <App />
   </React.StrictMode>
 );
