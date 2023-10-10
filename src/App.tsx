9d596654a46165905beceaf1d86906b9420ba575
hattori ryuki <ryuki.97913795@i.softbank.jp>
2023-10-10 15:04:40 +0900

    add: react-ga4を追加し、SPA用の設定を追記

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { useEffect } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
+import ReactGA from "react-ga4";
 
 import theme from "./theme/theme";
 import { Router } from "./router/Router";
 
 function App() {
+  ReactGA.initialize(`${process.env.REACT_APP_TRACKING_ID}`);
+
+  useEffect(() => {
+    ReactGA.send({
+      hitType: "pageview",
+      page: window.location.pathname + window.location.search,
+      title: "Home Page",
+    });
+  }, [window.location.pathname]);
+
   return (
     <ChakraProvider theme={theme}>
       <RecoilRoot>
